c6453dd0fde05ba07416912eba279f7bedb63f42
Marcela Barella <marcela.barella@hotmail.com>
2019-10-29 20:03:38 -0300

    add networks

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,9 +1,26 @@
 from bottle import route, run, request
+import psycopg2
+
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (subject, message))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Registered Message!')
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
+
+    register_message(subject, message)
+
     return f'Queued message! Subject: {subject}, Message: {message}'
 
 if __name__ == '__main__':
