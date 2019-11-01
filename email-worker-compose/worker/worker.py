28874d8a9395b4a69998ff57efed00278dd33514
Marcela Barella <marcela.barella@hotmail.com>
2019-10-31 21:36:55 -0300

    add dockerfile

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -6,6 +6,7 @@ import redis
 
 if __name__ == '__main__':
     red = redis.Redis(host='queue', port=6379, db=0)
+    print('Waiting messages...')
     while True:
         message = json.loads(red.blpop('sender')[1])
         # Simulating e-mail sending
